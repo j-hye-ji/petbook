1ce45b1de51f73cecd79d360123bde90f7ed4f8c
Junghyeji <arang030@naver.com>
2025-01-30 17:27:01 +0900

    initial commit

---
+++ src/test/java/com/hyeji/petbook/PetbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeji.petbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
