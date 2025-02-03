ce928a5e8071309c126498f68bea5529b1757e98
Junghyeji <arang030@naver.com>
2025-02-03 14:07:29 +0900

    사용자 회원가입 및 모든 사용자 조회 API

--- src/main/java/com/hyeji/petbook/controller/UserController.java
+++ src/main/java/com/hyeji/petbook/controller/UserController.java
@@ -1,18 +1,37 @@
 package com.hyeji.petbook.controller;
 
+import com.hyeji.petbook.dto.UserDTO;
+import com.hyeji.petbook.entity.User;
+import com.hyeji.petbook.repository.UserRepository;
 import com.hyeji.petbook.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
         this.userService = userService;
+    }
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody UserDTO userDTO) {
+        String message = userService.signUp(userDTO);
+        return ResponseEntity.ok(message);
+    }
+
+    // 모든 회원 조회
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 
 }
--- src/main/java/com/hyeji/petbook/service/UserService.java
+++ src/main/java/com/hyeji/petbook/service/UserService.java
@@ -1,8 +1,34 @@
 package com.hyeji.petbook.service;
 
+import com.hyeji.petbook.dto.UserDTO;
+import com.hyeji.petbook.entity.User;
+import com.hyeji.petbook.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    // 회원가입
+    public String signUp(UserDTO userDTO) {
+        String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+
+        User user = new User();
+        user.setUserName(userDTO.getUserName());
+        user.setPassword(encodedPassword);
+        user.setEmail(userDTO.getEmail());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+
+        userRepository.save(user);
+
+        return "회원가입 성공";
+    }
 }
