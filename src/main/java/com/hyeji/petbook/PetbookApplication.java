c96f153b4581ae701d1bd7645a0300ab8221546c
Junghyeji <arang030@naver.com>
2025-04-28 23:17:01 +0900

    PostSearchRepository 삭제 및 Spring Data Elasticsearch 방식 제거

--- src/main/java/com/hyeji/petbook/PetbookApplication.java
+++ src/main/java/com/hyeji/petbook/PetbookApplication.java
@@ -2,14 +2,12 @@ package com.hyeji.petbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-@EnableElasticsearchRepositories(basePackages = "com.hyeji.petbook.repository")
 public class PetbookApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hyeji/petbook/document/PostDocument.java
+++ src/main/java/com/hyeji/petbook/document/PostDocument.java
@@ -3,6 +3,9 @@ package com.hyeji.petbook.document;
 import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+
+import static org.springframework.data.elasticsearch.annotations.FieldType.Text;
 
 @Getter
 @Setter
@@ -15,6 +18,9 @@ public class PostDocument {
     @Id
     private String id;
 
+    @Field(type = Text)
     private String title;
+
+    @Field(type = Text)
     private String contents;
 }
